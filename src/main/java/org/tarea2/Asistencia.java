9183d30d48f2bce9a1274e3d0ddb9bc933eabfb0
zMRGABOz <gacastillo2023@udec.cl>
2024-05-03 22:18:09 -0400

    Adicion de tests

--- src/main/java/org/tarea2/Asistencia.java
+++ src/main/java/org/tarea2/Asistencia.java
@@ -1,4 +1,19 @@
 package org.tarea2;
 
+import java.awt.event.WindowStateListener;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Asistencia {
+    private List<Empleado> empleados;
+    private List<Empleado> atrasados;
+    private List<Empleado> ausentes;
+
+    public Asistencia(List empleados){
+        this.empleados = empleados;
+        this.atrasados = new ArrayList<>();
+        this.ausentes = new ArrayList<>();
+    }
+
 }
