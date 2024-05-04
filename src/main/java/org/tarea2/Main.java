4f9a46118d2781eb33e3e0790eb466eef9cc5487
StaCKm29 <mamartinez2023@udec.cl>
2024-05-03 22:56:48 -0400

    Creando empleados de prueba en main

--- src/main/java/org/tarea2/Main.java
+++ src/main/java/org/tarea2/Main.java
@@ -2,6 +2,17 @@ package org.tarea2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Empleado empleado1 = new Empleado("1", "Juan", "Perez", "jperez@udec.cl");
+        Empleado empleado2 = new Empleado("2", "María", "López", "mlopez@udec.cl");
+        Empleado empleado3 = new Empleado("3", "Pedro", "González", "pgonzalez@udec.cl");
+        Empleado empleado4 = new Empleado("4", "Ana", "Martínez", "amartinez@udec.cl");
+        Empleado empleado5 = new Empleado("5", "Carlos", "Rodríguez", "crodriguez@udec.cl");
+        Empleado empleado6 = new Empleado("6", "Laura", "Sánchez", "lsanchez@udec.cl");
+        Empleado empleado7 = new Empleado("7", "Diego", "Gómez", "dgomez@udec.cl");
+        Empleado empleado8 = new Empleado("8", "Sofía", "Hernández", "shernandez@udec.cl");
+        Empleado empleado9 = new Empleado("9", "Alejandro", "Díaz", "adiaz@udec.cl");
+        Empleado empleado10 = new Empleado("10", "Valentina", "Torres", "vtorres@udec.cl");
+
+
     }
 }
