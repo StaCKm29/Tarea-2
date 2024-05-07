4f8e18484e6d146dfa33c544a78abc61844e792f
zMRGABOz <gacastillo2023@udec.cl>
2024-05-07 17:28:12 -0400

    Adicion de DepartamentoTest

---
+++ src/test/java/org/tarea2/DepartamentoTest.java
@@ -0,0 +1,25 @@
+package org.tarea2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartamentoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addEmpleado() {
+    }
+
+    @Test
+    void obtenerCantidadEmpleados() {
+    }
+
+    @Test
+    void invitar() {
+    }
+}
