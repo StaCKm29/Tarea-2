757e46af5c7eab1ad01bd244b42b895d3ae301ff
StaCKm29 <mamartinez2023@udec.cl>
2024-05-07 17:25:22 -0400

    Creando carpeta para test correctamente

---
+++ src/test/java/org/tarea2/EmpleadoTest.java
@@ -0,0 +1,11 @@
+package pruebas;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class EmpleadoTest {
+    @Test
+    public void testEmpleado() {
+        Empleado empleado = new Empleado("1", "Juan", "Perez", "juperes@udec.cl");
+    }
+}
