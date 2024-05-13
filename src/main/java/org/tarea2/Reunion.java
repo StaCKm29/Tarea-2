05cd7b7e33b095ca318aada37c5331321229bc59
zMRGABOz <gacastillo2023@udec.cl>
2024-05-12 20:35:10 -0400

    Adicion de comentarios a reunion

--- src/main/java/org/tarea2/Reunion.java
+++ src/main/java/org/tarea2/Reunion.java
@@ -6,6 +6,10 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que representa una reunión, que será utilizada para gestionar las reuniones de la empresa.
+
+ */
 abstract class Reunion {
     private Date fecha;
     private Instant horaPrevista;
@@ -26,7 +30,14 @@ abstract class Reunion {
     private List <Retraso> retrasos;
     private List <String> mensajes;
 
-
+    /**
+     * Constructor de la clase Reunion
+     * @param tipoReunion Es el tipo de reunion (marketing, desarrollo, etc.)
+     * @param fecha Fecha de la reunión
+     * @param horaPrevista Hora prevista de la reunión
+     * @param duracionPrevista Duración prevista de la reunión
+     * @param listaInvitados Lista de empleados invitados a la reunión
+     */
     public Reunion(int tipoReunion, Date fecha, Instant horaPrevista, Duration duracionPrevista, List <Empleado> listaInvitados){
         this.tipoReunion = TipoReunion.values()[tipoReunion];
         this.fecha = fecha;
@@ -47,12 +58,21 @@ abstract class Reunion {
         }
     }
 
+    /**
+     * Método que obtiene la lista de asistentes a la reunión.
+     * @return Lista de asistentes a la reunión.
+     */
     public List obtenerAsistencias(){
         for (Asistencia as : totalAsistencias){
             empleadosAsistentes.add(as.getEmpleado());
         }
         return empleadosAsistentes;
     }
+
+    /**
+     * Método que obtiene la lista de ausentes a la reunión.
+     * @return Lista de ausentes a la reunión.
+     */
     public List obtenerAusencias() {
         for (Empleado empleado : listaInvitados) {
             boolean asistio = false;
@@ -72,33 +92,63 @@ abstract class Reunion {
         return empleadosAusentes;
     }
 
+    /**
+     * Método que obtiene la lista de empleados que llegaron tarde a la reunión.
+     * @return Lista de empleados que llegaron tarde a la reunión.
+     */
     public List obtenerRetrasos(){
         for(Retraso retraso : retrasos){
             empleadosAtrasados.add(retraso.getEmpleado());
         }
         return empleadosAtrasados;
     }
+
+    /**
+     * Método que obtiene el total de asistentes a la reunión.
+     * @return Total de asistentes a la reunión.
+     */
     public int obtenerTotalAsistencia(){
         return totalAsistentes;
     }
+
+    /**
+     * Método que obtiene el porcentaje de asistencia a la reunión.
+     * @return Porcentaje de asistencia a la reunión.
+     */
     public float obtenerPorcentajeAsistencia(){
         porcentajeAsistencia = ((float)totalAsistentes/listaInvitados.size())*100;
         String porcentajeString = String.valueOf(porcentajeAsistencia).replace(',', '.');
         porcentajeAsistencia = Float.parseFloat(porcentajeString);
         return porcentajeAsistencia;
     }
+
+    /**
+     * Método que calcula la duración real de la reunión.
+     * @return Duración real de la reunión.
+     */
     public Duration calcularTiempoReal(){
         duracionReal = Duration.between(horaInicio, horaFin);
         return duracionReal;
     }
 
+    /**
+     * Método que inicia la reunión.
+     */
     public void iniciar(){
         horaInicio = Instant.now();
     }
+
+    /**
+     * Método que finaliza la reunión.
+     */
     public void finalizar(){
         horaFin = Instant.now();
     }
 
+    /**
+     * Método que añade un empleado a la reunión.
+     * @param em Empleado a añadir a la reunión.
+     */
     public void empleadoEntrando(Empleado em){
         Asistencia asistio;
         if(horaInicio == null){
@@ -113,11 +163,20 @@ abstract class Reunion {
         totalAsistentes++;
     }
 
+
+    /**
+     * Método que añade una nota a la reunión.
+     * @param mensaje Mensaje de la nota.
+     */
     public void nuevaNota(String mensaje){
         Nota nota = new Nota(mensaje);
         almacenNotas.add(nota);
     }
 
+    /**
+     * Método que obtiene las notas de la reunión.
+     * @return Notas de la reunión.
+     */
     public String getNotas(){
         mensajes = new ArrayList<>();
         for(Nota notita : almacenNotas){
@@ -126,28 +185,57 @@ abstract class Reunion {
         return mensajes.toString();
     }
 
+    /**
+     * Método que obtiene la fecha de la reunión.
+     * @return Fecha de la reunión.
+     */
     public Date getFecha(){
         return fecha;
     }
+
+    /**
+     * Método que obtiene la hora prevista de la reunión.
+     * @return Hora prevista de la reunión.
+     */
     public Instant getHoraPrevista(){
         return horaPrevista;
     }
 
+    /**
+     * Método que obtiene la hora de inicio de la reunión.
+     * @return La hora de inicio de la reunión.
+     */
     public Instant getHoraInicio(){
         return horaInicio;
     }
 
+    /**
+     * Método que obtiene la hora de fin de la reunión.
+     * @return La hora de fin de la reunión.
+     */
     public Instant getHoraFin(){
         return horaFin;
     }
 
+    /**
+     * Método que obtiene la duración real de la reunión.
+     * @return La duración real de la reunión.
+     */
     public Duration getDuracionReal(){
         return duracionReal;
     }
 
+    /**
+     * Método que obtiene el tipo de reunión.
+     * @return Tipo de reunión.
+     */
     public TipoReunion getTipoReunion(){
         return tipoReunion;
     }
 
+    /**
+     * Método abstracto para obtener la sala/enlace de la reunión ya sea presencial o virtual.
+     * @return Sala/enlace de la reunión.
+     */
     public abstract String getSalaEnlace();
 }
